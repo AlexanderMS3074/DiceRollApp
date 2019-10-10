56db00a0fb1134f83b77b2ff3cd47926ff21379c
AlexanderMS3074 <alex.smith3074@gmail.com>
2019-10-10 16:01:11 -0500

    Initial commit

---
+++ DiceRollApp/MainWindow.xaml.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace DiceRollApp {
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window {
+        private int turnCount = 0;
+        private string rollResults;
+        private string rollModifiers;
+
+        public MainWindow() {
+            InitializeComponent();
+            tblkRollHistory.Text = "I'm working!!!";
+        }
+
+        private void BtnTest_Click(object sender, RoutedEventArgs e) {
+            tblkRollHistory.Text = tblkRollHistory.Text; //TODO change this!!!!!
+        }
+    }
+}
